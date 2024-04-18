1e6ded0be4ee56df586aed4b036c804b37cc82f8
mayuripy <115979827+mayuripy@users.noreply.github.com>
2024-04-18 11:56:27 +0530

    install .env

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const dotenv =require("dotenv").config();
 
 const app =express();
 
 
-const port=5000;
+const port=process.env.PORT ||5000;
+
+app.get("/api/contacts",(req,res)=>{
+    res.send("get all contacts !")
+});
 
 app.listen(port,() =>{
     console.log(`server is running  ${port}`);
